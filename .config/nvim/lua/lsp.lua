c9eea8be9c8c851858412ee1f57602027d9095bf
EliaECoyote <elia.camposilvan@gmail.com>
2021-04-24 16:15:17 +0200

    Lint lsp.lua

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,17 +1,17 @@
-local lspconfig = require'lspconfig'
-local util = require'lspconfig/util'
-local lsp_status = require'lsp-status'
+local lspconfig = require"lspconfig"
+local util = require"lspconfig/util"
+local lsp_status = require"lsp-status"
 
 lsp_status.register_progress()
 
 lsp_status.config {
   current_function = false,
-  indicator_errors = '❌',
-  indicator_warnings = '⚠️ ',
-  indicator_info = 'ℹ️ ',
-  indicator_hint = '❗',
-  indicator_ok = 'All good!',
-  status_symbol = '',
+  indicator_errors = "❌",
+  indicator_warnings = "⚠️ ",
+  indicator_info = "ℹ️ ",
+  indicator_hint = "❗",
+  indicator_ok = "All good!",
+  status_symbol = "",
 }
 
 local system_name
@@ -19,7 +19,7 @@ if vim.fn.has("mac") == 1 then
   system_name = "macOS"
 elseif vim.fn.has("unix") == 1 then
   system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
+elseif vim.fn.has("win32") == 1 then
   system_name = "Windows"
 else
   print("Unsupported system for sumneko")
@@ -88,7 +88,7 @@ lspconfig.vimls.setup {
   capabilities = lsp_status.capabilities,
 }
 
-local sumneko_root_path = '/Volumes/Projects/lua-language-server'
+local sumneko_root_path = "/Volumes/Projects/lua-language-server"
 local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
 lspconfig.sumneko_lua.setup {
@@ -96,21 +96,21 @@ lspconfig.sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you're using
+        -- Tell the language server which version of Lua you"re using
         -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
         -- Setup your lua path
-        path = vim.split(package.path, ';'),
+        path = vim.split(package.path, ";"),
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = {"vim"},
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
         },
       },
       -- Do not send telemetry data containing a randomized but unique identifier
