a947c7a4efe4a0c42b14fb36d284cf5fefff9ea8
EliaECoyote <elia.camposilvan@gmail.com>
2021-05-06 21:57:34 +0200

    Move lsp bindings to lsp.lua

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -150,25 +150,6 @@ set smartcase
 
 " }}}
 
-" LSP setup {{{
-
-lua require('lsp')
-
-nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
-nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
-nnoremap <silent> gi    <cmd>lua vim.lsp.buf.implementation()<CR>
-nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
-nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
-nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
-nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
-nnoremap <silent> gd    <cmd>lua vim.lsp.buf.declaration()<CR>
-nnoremap <silent> F2    <cmd>lua vim.lsp.buf.rename()<CR>
-nnoremap <silent> F12   <cmd>lua vim.lsp.buf.formatting()<CR>
-nnoremap <silent> <leader>a <cmd>lua vim.lsp.buf.code_action()<CR>
-
-" }}}
-
 " Statusline {{{
 
 " Hides 'mode' label on the last line
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -14,6 +14,30 @@ lsp_status.config {
   status_symbol = "",
 }
 
+local function custom_attach(client)
+  print("LSP 🙏")
+  lsp_status.on_attach(client)
+  print("LSP 🙏")
+
+  local set_keymap = vim.api.nvim_set_keymap
+  local options = { silent = true, noremap = true }
+
+  set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", options)
+  set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", options)
+  set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", options)
+  set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", options)
+  set_keymap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", options)
+  set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", options)
+  set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", options)
+  set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", options)
+  set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>", options)
+  set_keymap("n", "F2", "<cmd>lua vim.lsp.buf.rename()<cr>", options)
+  set_keymap("n", "F12", "<cmd>lua vim.lsp.buf.formatting()<cr>", options)
+  set_keymap("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<cr>", options)
+  set_keymap("n", "]g", "<cmd>lua vim.lsp.diagnostic.goto_next()<cr>", options)
+  set_keymap("n", "[g", "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", options)
+end
+
 local eslint = {
   lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
   lintStdin = true,
@@ -39,7 +63,7 @@ lspconfig.efm.setup {
   root_dir = util.root_pattern(".git", vim.fn.getcwd()),
   on_attach = function(client)
     client.resolved_capabilities.document_formatting = true
-    lsp_status.on_attach(client)
+    custom_attach(client)
   end,
   capabilities = lsp_status.capabilities,
   filetypes = {
@@ -67,21 +91,21 @@ lspconfig.tsserver.setup {
   on_attach = function (client)
     -- Disable ts builtin formatting
     client.resolved_capabilities.document_formatting = false
-    lsp_status.on_attach(client)
+    custom_attach(client)
   end,
   capabilities = lsp_status.capabilities,
 }
 
 lspconfig.vimls.setup {
-  on_attach = lsp_status.on_attach,
+  on_attach = custom_attach,
   capabilities = lsp_status.capabilities,
 }
 
-local jls_root_path = "/Volumes/Projects/java-language-server"
-local jls_binary = jls_root_path.."/dist/lang_server_mac.sh"
+local javals_root_path = "/Volumes/Projects/java-language-server"
+local javals_binary = javals_root_path.."/dist/lang_server_mac.sh"
 lspconfig.java_language_server.setup {
-  cmd = { jls_binary },
-  on_attach = lsp_status.on_attach,
+  cmd = { javals_binary },
+  on_attach = custom_attach,
   capabilities = lsp_status.capabilities,
 }
 
@@ -115,6 +139,6 @@ lspconfig.sumneko_lua.setup {
       },
     },
   },
-  on_attach = lsp_status.on_attach,
+  on_attach = custom_attach,
   capabilities = lsp_status.capabilities,
 }
