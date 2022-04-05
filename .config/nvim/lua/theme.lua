cb5237d521f26a036083e83557fc9d1889291022
EliaECoyote <elia.camposilvan@gmail.com>
2022-04-05 23:24:24 +0200

    Improve windows UI bg / borders

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -7,6 +7,8 @@ vim.api.nvim_command([[
 vim.api.nvim_set_option("background", "dark")
 
 -- `:h gruvbox-material.txt` for more details
+
+-- Sets contrast
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_palette = "original"
 vim.api.nvim_command("colorscheme gruvbox-material")
@@ -35,18 +37,36 @@ vim.api.nvim_command("hi EndOfBuffer guibg=none")
 -- Use default terminal bg color
 vim.api.nvim_command("hi Normal guibg=none")
 -- Cusomize vimdiff colors
-vim.api.nvim_command("hi DiffAdd      gui=none    guifg=NONE          guibg=#23414f")
-vim.api.nvim_command("hi DiffChange   gui=none    guifg=NONE          guibg=#383725")
+vim.api.nvim_command("hi DiffAdd      gui=none    guifg=none          guibg=#23414f")
+vim.api.nvim_command("hi DiffChange   gui=none    guifg=none          guibg=#383725")
 vim.api.nvim_command("hi DiffDelete   gui=none    guifg=#3d2b28       guibg=#3d2b28")
-vim.api.nvim_command("hi DiffText     gui=none    guifg=NONE          guibg=#454425")
+vim.api.nvim_command("hi DiffText     gui=none    guifg=none          guibg=#454425")
+
+-- cf. https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
+-- Sets transparent bg for LSP floating windows.
+vim.api.nvim_command("hi NormalFloat guibg=none")
+-- Links LSP floating windows border color with telescope border color.
+vim.api.nvim_command("hi link FloatBorder TelescopeBorder")
+
+local win = require('lspconfig.ui.windows')
+local _default_opts = win.default_opts
+
+win.default_opts = function(options)
+  local opts = _default_opts(options)
+  opts.border = 'single'
+  return opts
+end
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
   vim.lsp.handlers.hover,
-  { border = "single" }
+  { border = "rounded" }
 )
-
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = "rounded" }
+)
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
   vim.lsp.handlers.signature_help,
-  { border = "single" }
+  { border = "rounded" }
 )
 
