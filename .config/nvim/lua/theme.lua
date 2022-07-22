4d76d5ee81f90c2b25ea9c9a3bfb378563358e0d
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2022-07-23 00:09:05 +0200

    Theme update

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -4,51 +4,86 @@ vim.api.nvim_command([[
   endif
 ]])
 
-vim.api.nvim_set_option("background", "dark")
+-- Temporarily highlights yanked region after each yank
+vim.api.nvim_command([[
+ au TextYankPost * silent! lua vim.highlight.on_yank()
+]])
 
--- `:h gruvbox-material.txt` for more details
+-- Enables text wrap
+vim.opt.linebreak = true
+vim.opt.wrap = true
 
--- Sets contrast
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_palette = "original"
-vim.api.nvim_command("colorscheme gruvbox-material")
+-- Disables hard-wrap
+vim.opt.textwidth = 0
 
--- Soft line wrap
-vim.api.nvim_command("set linebreak wrap tw=0")
 -- Makes vimdiff easier to read
-vim.api.nvim_command("set diffopt+=algorithm:patience")
-vim.api.nvim_command("set diffopt+=vertical")
+vim.opt.diffopt:append("algorithm:patience")
+vim.opt.diffopt:append("vertical")
+
 -- set diffopt+=indent-heuristic
-vim.api.nvim_command("set cursorline")
-vim.api.nvim_command("set relativenumber")
-vim.api.nvim_command("set number")
-vim.api.nvim_command("set updatetime=300")
-vim.api.nvim_command("set shortmess+=c")
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.updatetime = 300
+vim.opt.shortmess:append("c")
 
-vim.api.nvim_command("hi LineNr guifg=#5eacd3")
-vim.api.nvim_command("hi CursorLine guibg=#4f4f4f")
-vim.api.nvim_command("hi CursorLineNr guibg=none guifg=#ff8533")
-vim.api.nvim_command("hi Visual guibg=#434f4e")
-vim.api.nvim_command("hi MatchParen guibg=#c90000")
--- Use default terminal bg colors
-vim.api.nvim_command("hi SignColumn guibg=none")
-vim.api.nvim_command("hi NonText guibg=none")
-vim.api.nvim_command("hi EndOfBuffer guibg=none")
--- Use default terminal bg color
-vim.api.nvim_command("hi Normal guibg=none")
--- Cusomize vimdiff colors
-vim.api.nvim_command("hi DiffAdd      gui=none    guifg=none          guibg=#23414f")
-vim.api.nvim_command("hi DiffChange   gui=none    guifg=none          guibg=#383725")
-vim.api.nvim_command("hi DiffDelete   gui=none    guifg=#3d2b28       guibg=#3d2b28")
-vim.api.nvim_command("hi DiffText     gui=none    guifg=none          guibg=#454425")
 
--- For LSP-related UI customzations, see
--- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
+-- Highlights end-of-line
+vim.opt.list = true
+vim.opt.listchars = { space = " ", tab = "->", eol = "¬" }
 
--- Sets transparent bg for LSP floating windows.
-vim.api.nvim_command("hi NormalFloat guibg=none")
--- Links LSP floating windows border color with telescope border color.
-vim.api.nvim_command("hi link FloatBorder TelescopeBorder")
+local function setupStandardHighlights()
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#5eacd3" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#ff8533" })
+  vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+  -- Make bg transparent, to display the default terminal bg color
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+
+  -- For LSP-related UI customzations, see
+  -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
+
+  -- Sets transparent bg for LSP floating windows.
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  -- Links LSP floating windows border color with telescope border color.
+  vim.api.nvim_set_hl(0, "FloatBorder", { link = "TelescopeBorder" })
+end
+
+local function setupDarkTheme()
+  vim.g.gruvbox_material_background = "hard"
+  vim.g.gruvbox_material_palette = "original"
+
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = "#4f4f4f" })
+  vim.api.nvim_set_hl(0, "Visual", { bg = "#434f4e" })
+  vim.api.nvim_set_hl(0, "NonText", { fg = "#4a4a4a", bg = "none" })
+  vim.api.nvim_set_hl(0, "MatchParen", { bg = "#c90000" })
+
+  -- Cusomize vimdiff colors
+  vim.api.nvim_set_hl(0, "DiffAdd", { underline = 0, standout = 0, fg = "none", bg = "#23414f" })
+  vim.api.nvim_set_hl(0, "DiffChange", { underline = 0, standout = 0, fg = "none", bg = "#383725" })
+  vim.api.nvim_set_hl(0, "DiffDelete", { bold = 0, fg = "#3d2b28", bg = "#3d2b28" })
+  vim.api.nvim_set_hl(0, "DiffText", { underline = 0, standout = 0, fg = "none", bg = "#454425" })
+  vim.api.nvim_command("colorscheme gruvbox-material")
+end
+
+local function setupLightTheme()
+  vim.g.gruvbox_material_background = "medium"
+  vim.g.gruvbox_material_palette = "original"
+  vim.api.nvim_command("colorscheme gruvbox-material")
+end
+
+vim.api.nvim_set_option("background", "light")
+local background = vim.opt.background:get()
+
+if (background == "light") then
+  setupLightTheme()
+else
+  setupDarkTheme()
+end
+setupStandardHighlights()
 
 -- Enable floating window borders by default
 local original_open_floating_preview = vim.lsp.util.open_floating_preview
@@ -56,6 +91,16 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or "rounded"
   return original_open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- Enable window borders by default
+-- (enables border for commands like `LspInfo`)
+local win = require("lspconfig.ui.windows")
+local _default_opts = win.default_opts
+function win.default_opts(options)
+  local opts = _default_opts(options)
+  opts.border = "rounded"
+  return opts
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
