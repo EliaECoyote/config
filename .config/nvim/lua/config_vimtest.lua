a31e6b3504e6d3addd0ad15c7b3ac3cf0e150f46
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2022-08-04 22:24:15 +0200

    Fix vimtest for jest tests

--- .config/nvim/lua/config_vimtest.lua
+++ .config/nvim/lua/config_vimtest.lua
@@ -3,7 +3,11 @@ vim.g["test#strategy"] = "neovim"
 vim.g["test#neovim#start_normal"] = 1
 vim.g["test#neovim#term_position"] = "vert botright split"
 vim.g["test#python#runner"] = "pytest"
+vim.g["test#javascript#runner"] = "jest"
+vim.g["test#javascript#jest#executable"] = "yarn test:unit"
+vim.g["test#javascript#jest#file_pattern"] = "\\v(__tests__/.*|(spec|test|unit))\\.(js|jsx|coffee|ts|tsx)$"
 
+-- Make it work with dogweb
 vim.g["test#python#pytest#options"] = "--with-pylons $DATADOG_ROOT/dogweb/test.ini --doctest-modules --disable-warnings"
 
 local function pytest_transform(cmd)
@@ -13,7 +17,7 @@ end
 vim.g['test#custom_transformations'] = { pytest_transform }
 vim.g['test#transformation'] = 'pytest_transform'
 
-vim.api.nvim_set_keymap("n", "<leader>tn", "<cmd>TestNearest<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>tl", "<cmd>TestLast<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>TestFile<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>TestVisit<cr>", {})
+vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", {})
+vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>", {})
+vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>", {})
+vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<cr>", {})
