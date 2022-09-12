07affdfc4e2943e556822974c59abd19481fc6fa
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2022-09-12 23:25:27 +0200

    Telescope config fixes

--- .config/nvim/lua/config_telescope.lua
+++ .config/nvim/lua/config_telescope.lua
@@ -1,121 +1,40 @@
-local utils_file = require("lib.utils_file")
-local utils_buffer = require("lib.utils_buffer")
-local telescope = require("telescope")
-local terminal = require("toggleterm.terminal")
+local utils_file    = require("lib.utils_file")
+local utils_buffer  = require("lib.utils_buffer")
+local telescope     = require("telescope")
+local terminal      = require("toggleterm.terminal")
 local entry_display = require("telescope.pickers.entry_display")
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local builtin = require("telescope.builtin")
-local config = require("telescope.config")
-local themes = require("telescope.themes")
-local actions = require("telescope.actions")
-local previewer_utils = require("telescope.previewers.utils")
-local action_state = require("telescope.actions.state")
-local fb_actions = require("telescope").extensions.file_browser.actions
+local pickers       = require("telescope.pickers")
+local finders       = require("telescope.finders")
+local builtin       = require("telescope.builtin")
+local config        = require("telescope.config")
+local themes        = require("telescope.themes")
+local actions       = require("telescope.actions")
+local action_state  = require("telescope.actions.state")
+local make_entry    = require("telescope.make_entry")
 
-telescope.setup({
-  defaults = {
-    vimgrep_arguments = {
-      "rg", "--color=never", "--no-heading", "--with-filename", "--line-number",
-      "--column", "--smart-case",
-    },
-    -- layout_config = {
-    --   vertical = { height = 0.9, width = 0.9, preview_cutoff = 10 },
-    -- },
-    -- path_display = { "truncate", "absolute" },
-  },
-  pickers = {
-    buffers = {
-      theme = "ivy",
-      path_display = { "truncate", "absolute" },
-      attach_mappings = function(prompt_bufnr, map)
-        map("n", "bd", function()
-          local current_picker = action_state.get_current_picker(prompt_bufnr)
-          current_picker:delete_selection(function(selection)
-            utils_buffer.delete_buffer(selection.bufnr, { force = true, unload = true }, false)
-          end)
-        end)
-        return true
-      end,
-    },
-    live_grep = {
-      path_display = { "smart", "absolute" },
-    },
-    git_files = {
-      theme = "ivy",
-      path_display = { "smart", "absolute" }
-    },
-  },
-  oldfiles = {
-    theme = "ivy",
-    path_display = { "truncate", "absolute" },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    },
-    file_browser = {
-      theme = "ivy",
-      hidden = true,
-      hijack_netrw = true,
-      path = "%:p:h",
-      -- path_display = { "tail" },
-      -- path_display = { "smart" },
-      mappings = {
-        ["i"] = {
-          ["<M-c>"] = false,
-          ["<M-r>"] = false,
-          ["<M-m>"] = false,
-          ["<M-y>"] = false,
-          ["<M-d>"] = false,
-          ["<C-o>"] = false,
-          ["<C-g>"] = false,
-          ["<C-e>"] = false,
-          ["<C-w>"] = false,
-          ["<C-t>"] = false,
-          ["<C-f>"] = false,
-          ["<C-h>"] = false,
-          ["<C-s>"] = false,
-          ["<S-CR>"] = false,
-        },
-        ["n"] = {
-          h = false,
-          c = false,
-          r = false,
-          d = false,
-          o = false,
-          g = false,
-          e = false,
-          w = false,
-          t = false,
-          f = false,
-          s = false,
-          ["<S-CR>"] = false,
-          ["<C-o>"] = fb_actions.create,
-          ["<C-O>"] = fb_actions.open,
-          ["<C-c>"] = fb_actions.rename,
-          ["<C-p>"] = fb_actions.move,
-          ["<C-y>"] = fb_actions.copy,
-          ["<C-d>"] = fb_actions.remove,
-          ["<C-w>"] = fb_actions.goto_cwd,
-          ["<C-W>"] = fb_actions.goto_cwd,
-          ["<C-f>"] = fb_actions.toggle_browser,
-          ["<C-s>"] = fb_actions.toggle_all,
-          ["<C-h>"] = fb_actions.goto_parent_dir,
-        },
-      },
-    },
-  },
-})
+local file_browser_actions = require("telescope").extensions.file_browser.actions
 
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("live_grep_args")
-require("telescope").load_extension("file_browser")
+local telescope_defaults = {
+  theme = "ivy",
+  path_display = { "truncate" },
+  vimgrep_arguments = {
+    "rg", "--color=never", "--no-heading", "--with-filename", "--line-number",
+    "--column", "--smart-case", "--trim",
+  },
+}
 
-local function bookmarks()
+local function set_current_folder_as_cwd(prompt_bufnr)
+  local file_browser_utils = require("telescope._extensions.file_browser.utils")
+  local current_picker = action_state.get_current_picker(prompt_bufnr)
+  local finder = current_picker.finder
+  finder.cwd = finder.path
+  vim.cmd("cd " .. finder.path)
+  file_browser_utils.redraw_border_title(current_picker)
+  current_picker:refresh(finder, { reset_prompt = true, multi = current_picker._multi })
+  print("CWD changed to " .. finder.path)
+end
+
+local function bookmarks(opts)
   local files = {
     "~/.tmux.conf", "~/.config/vifm/vifmrc",
     "~/.config/karabiner/karabiner.json", "~/.gitconfig", "~/.gitignore",
@@ -130,16 +49,19 @@ local function bookmarks()
     "/Volumes/Projects/playground/"
   }
   local folder_files = utils_file.scan_deep_files(folders)
-  for _, path in ipairs(folder_files) do table.insert(files, path) end
-  local opts = {
-    path_display = { "tail" }
-  }
-  pickers.new(opts, {
-    prompt_title = "Bookmarks",
-    finder = finders.new_table(files),
-    previewer = config.values.file_previewer(opts),
-    sorter = config.values.file_sorter(opts),
-  }):find()
+  for _, path in ipairs(folder_files) do
+    table.insert(files, path)
+  end
+  opts = opts or {}
+  opts.prompt_title = "Bookmarks"
+  opts.path_display = { "truncate" }
+  opts.finder = finders.new_table({
+    results = files,
+    entry_maker = make_entry.gen_from_file(opts)
+  })
+  opts.previewer = config.values.file_previewer(opts)
+  opts.sorter = config.values.file_sorter(opts)
+  pickers.new(themes.get_ivy(opts), telescope_defaults):find()
 end
 
 local function terminals()
@@ -200,6 +122,11 @@ local function terminals()
   }):find()
 end
 
+local function project_files(opts)
+  local ok = pcall(builtin.git_files, opts)
+  if not ok then builtin.find_files(opts) end
+end
+
 local function select_background(opts)
   local function load_iterm_background(file_name)
     local apple_script = string.format([[
@@ -233,20 +160,108 @@ local function select_background(opts)
   }):find()
 end
 
+telescope.setup({
+  defaults = telescope_defaults,
+  pickers = {
+    lsp_references = {
+      theme = "ivy",
+    },
+    lsp_definitions = {
+      theme = "ivy",
+    },
+    buffers = {
+      attach_mappings = function(prompt_bufnr, map)
+        map("n", "<C-x>", function()
+          local current_picker = action_state.get_current_picker(prompt_bufnr)
+          current_picker:delete_selection(function(selection)
+            utils_buffer.delete_buffer(selection.bufnr, { force = true, unload = true }, false)
+          end)
+        end)
+        return true
+      end,
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    },
+    live_grep_args = {
+      theme = "ivy",
+    },
+    file_browser = {
+      theme = "ivy",
+      hidden = true,
+      hijack_netrw = true,
+      path = "%:p:h",
+      mappings = {
+        ["i"] = {
+          ["<M-c>"] = false,
+          ["<M-r>"] = false,
+          ["<M-m>"] = false,
+          ["<M-y>"] = false,
+          ["<M-d>"] = false,
+          ["<C-o>"] = false,
+          ["<C-g>"] = false,
+          ["<C-e>"] = false,
+          ["<C-w>"] = false,
+          ["<C-t>"] = false,
+          ["<C-f>"] = false,
+          ["<C-h>"] = false,
+          ["<C-s>"] = false,
+          ["<S-CR>"] = false,
+        },
+        ["n"] = {
+          h = false,
+          c = false,
+          r = false,
+          d = false,
+          o = false,
+          g = false,
+          e = false,
+          w = false,
+          t = false,
+          f = false,
+          s = false,
+          ["<S-CR>"] = false,
+          ["<C-o>"] = file_browser_actions.create,
+          ["<C-cr>"] = file_browser_actions.open,
+          ["<C-c>"] = file_browser_actions.rename,
+          ["<C-p>"] = file_browser_actions.move,
+          ["<C-y>"] = file_browser_actions.copy,
+          ["<C-x>"] = file_browser_actions.remove,
+          ["<C-r>"] = file_browser_actions.goto_cwd,
+          ["<C-w>"] = set_current_folder_as_cwd,
+          ["<C-f>"] = file_browser_actions.toggle_browser,
+          ["<C-s>"] = file_browser_actions.toggle_all,
+          ["<C-h>"] = file_browser_actions.goto_parent_dir,
+        },
+      },
+    },
+  },
+})
+
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("live_grep_args")
+require("telescope").load_extension("file_browser")
+
 local options = { noremap = true }
 
 vim.keymap.set("n", "<leader>o", builtin.buffers, options)
 vim.keymap.set("n", "<leader>ff", builtin.live_grep, options)
-vim.keymap.set("n", "<leader>F", require("telescope").extensions.live_grep_args.live_grep_args, options)
-vim.keymap.set("n", "<leader>p", builtin.git_files, options)
+vim.keymap.set("n", "<leader>F", telescope.extensions.live_grep_args.live_grep_args, options)
+vim.keymap.set("n", "<leader>p", project_files, options)
 vim.keymap.set("n", "<leader>?", builtin.oldfiles, options)
 vim.keymap.set("n", "<leader>fq", builtin.quickfix, options)
 vim.keymap.set("n", "<leader>f?", builtin.builtin, options)
 vim.keymap.set("n", "<leader>fl", builtin.loclist, options)
-vim.keymap.set("n", "<leader>fb", require("telescope").extensions.file_browser.file_browser, options)
+vim.keymap.set("n", "<leader>fb", telescope.extensions.file_browser.file_browser, options)
 vim.keymap.set("n", "<leader>fw", bookmarks, options)
 vim.keymap.set("n", "<leader>ft", terminals, options)
 vim.keymap.set("n", "<leader>fB", select_background, options)
+vim.keymap.set("n", "<leader>fr", builtin.resume, options)
 
 -- LSP
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, options)
