75f20c985f500c7a9ab3eb3ca0a5076809393378
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2022-10-29 13:51:25 +0200

    Add lazygit toggleterm config

--- .config/nvim/lua/config_toggleterm.lua
+++ .config/nvim/lua/config_toggleterm.lua
@@ -1,4 +1,5 @@
 local toggleterm = require("toggleterm")
+local Terminal   = require("toggleterm.terminal").Terminal
 
 toggleterm.setup({
   open_mapping = "<c-\\>",
@@ -12,7 +13,7 @@ toggleterm.setup({
 
 local function set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', "<C-\\><C-n>", opts)
+  vim.keymap.set("t", "<esc>", "<C-\\><C-n>", opts)
   -- vim.keymap.set('t', '<C-h>', "<Cmd>wincmd h<CR>", opts)
   -- vim.keymap.set('t', '<C-j>', "<Cmd>wincmd j<CR>", opts)
   -- vim.keymap.set('t', '<C-k>', "<Cmd>wincmd k<CR>", opts)
@@ -33,5 +34,32 @@ vim.keymap.set(
   "n",
   "<c-\\>",
   "<Cmd>exe v:count1 . \"ToggleTerm\"<CR>",
+  { noremap = true, silent = true }
+)
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "rounded",
+    width = vim.o.columns,
+    height = 100,
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+  -- function to run on closing the terminal
+  on_close = function(_)
+    vim.cmd("startinsert!")
+  end,
+})
+
+vim.keymap.set(
+  "n",
+  "<leader>g",
+  function() lazygit:toggle() end,
   { noremap = true, silent = true }
 )
