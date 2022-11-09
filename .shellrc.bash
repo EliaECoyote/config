1a1e983ec7632efea8bb89f64082ce75dd1d3296
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2022-11-09 11:24:55 +0100

    Cleanup shellrc conf

--- .shellrc.bash
+++ .shellrc.bash
@@ -95,10 +95,10 @@ alias g='git'
 # Open PR {{{2
 
 function open_pr() {
-  github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
-  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
-  pr_url=$github_url"/compare/"$branch_name"?expand=1";
-  open $pr_url;
+  github_url=$(git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/');
+  branch_name=$(git symbolic-ref HEAD | cut -d"/" -f 3,4);
+  pr_url="$github_url/compare/$branch_name?expand=1";
+  open "$pr_url";
 }
 
 # }}}
