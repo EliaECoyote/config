1dbc8b6352c769fa661e8435dd7393151965df4d
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-03-06 18:10:43 +0100

    Cleanup telescope config

--- .config/nvim/lua/config_telescope.lua
+++ .config/nvim/lua/config_telescope.lua
@@ -1,7 +1,6 @@
 local utils_file    = require("lib.utils_file")
 local utils_buffer  = require("lib.utils_buffer")
 local telescope     = require("telescope")
-local entry_display = require("telescope.pickers.entry_display")
 local pickers       = require("telescope.pickers")
 local finders       = require("telescope.finders")
 local builtin       = require("telescope.builtin")
@@ -10,8 +9,6 @@ local themes        = require("telescope.themes")
 local actions       = require("telescope.actions")
 local action_state  = require("telescope.actions.state")
 local make_entry    = require("telescope.make_entry")
-
-local file_browser_actions = require("telescope").extensions.file_browser.actions
 
 local telescope_defaults = {
   theme = "ivy",
@@ -27,17 +24,6 @@ local telescope_defaults = {
     },
   },
 }
-
-local function set_current_folder_as_cwd(prompt_bufnr)
-  local file_browser_utils = require("telescope._extensions.file_browser.utils")
-  local current_picker = action_state.get_current_picker(prompt_bufnr)
-  local finder = current_picker.finder
-  finder.cwd = finder.path
-  vim.cmd("cd " .. finder.path)
-  file_browser_utils.redraw_border_title(current_picker)
-  current_picker:refresh(finder, { reset_prompt = true, multi = current_picker._multi })
-  print("CWD changed to " .. finder.path)
-end
 
 local function bookmarks(opts)
   local files = {
@@ -120,72 +106,20 @@ telescope.setup({
     live_grep_args = {
       theme = "ivy",
     },
-    file_browser = {
-      theme = "ivy",
-      path_display = { "smart" },
-      hidden = true,
-      path = "%:p:h",
-      mappings = {
-        ["i"] = {
-          ["<M-c>"] = false,
-          ["<M-r>"] = false,
-          ["<M-m>"] = false,
-          ["<M-y>"] = false,
-          ["<M-d>"] = false,
-          ["<C-o>"] = false,
-          ["<C-g>"] = false,
-          ["<C-e>"] = false,
-          ["<C-w>"] = false,
-          ["<C-t>"] = false,
-          ["<C-f>"] = false,
-          ["<C-h>"] = false,
-          ["<C-s>"] = false,
-          ["<S-CR>"] = false,
-        },
-        ["n"] = {
-          h = false,
-          c = false,
-          r = false,
-          d = false,
-          o = false,
-          g = false,
-          e = false,
-          w = false,
-          t = false,
-          f = false,
-          s = false,
-          ["<S-CR>"] = false,
-          ["<C-o>"] = file_browser_actions.create,
-          ["<C-cr>"] = file_browser_actions.open,
-          ["<C-c>"] = file_browser_actions.rename,
-          ["<C-p>"] = file_browser_actions.move,
-          ["<C-y>"] = file_browser_actions.copy,
-          ["<C-x>"] = file_browser_actions.remove,
-          ["<C-r>"] = file_browser_actions.goto_cwd,
-          ["<C-w>"] = set_current_folder_as_cwd,
-          ["<C-f>"] = file_browser_actions.toggle_browser,
-          ["<C-s>"] = file_browser_actions.toggle_all,
-          ["<C-h>"] = file_browser_actions.goto_parent_dir,
-        },
-      },
-    },
   },
 })
 
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("live_grep_args")
-require("telescope").load_extension("file_browser")
 
-local options = { noremap = true }
-
-vim.keymap.set("n", "<C-p>", function() builtin.find_files(themes.get_dropdown({ previewer = false })) end, options)
-vim.keymap.set("n", "<leader>o", builtin.buffers, options)
-vim.keymap.set("n", "<leader>ff", telescope.extensions.live_grep_args.live_grep_args, options)
-vim.keymap.set("n", "<leader>fo", builtin.oldfiles, options)
-vim.keymap.set("n", "<leader>f?", builtin.builtin, options)
-vim.keymap.set("n", "<leader>fw", bookmarks, options)
-vim.keymap.set("n", "<leader>fr", builtin.resume, options)
+vim.keymap.set("n", "<C-p>", function() builtin.find_files(themes.get_dropdown({ previewer = false })) end)
+vim.keymap.set("n", "<leader>o", builtin.buffers)
+vim.keymap.set("n", "<leader>ff", telescope.extensions.live_grep_args.live_grep_args)
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
+vim.keymap.set("n", "<leader>f?", builtin.builtin)
+vim.keymap.set("n", "<leader>fw", bookmarks)
+vim.keymap.set("n", "<leader>fr", builtin.resume)
 
 -- LSP
-vim.keymap.set("n", "gr", builtin.lsp_references, options)
-vim.keymap.set("n", "gd", builtin.lsp_definitions, options)
+vim.keymap.set("n", "gr", builtin.lsp_references)
+vim.keymap.set("n", "gd", builtin.lsp_definitions)
