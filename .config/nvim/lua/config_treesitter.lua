a4cf230fe8e71a69d359da1e2676f6745e0d6fe7
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-03-31 21:13:29 +0200

    Disable treesitter highlight for large files

--- .config/nvim/lua/config_treesitter.lua
+++ .config/nvim/lua/config_treesitter.lua
@@ -6,6 +6,9 @@ configs.setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
+    disable = function(_, bufnr)
+      return vim.api.nvim_buf_line_count(bufnr) > 5000
+    end,
   },
   indent = {
     enable = true,
