498c667f663ea83d47f014503f17dce9361c7add
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-16 23:19:22 +0200

    Add wezterm conf

---
+++ .wezterm.lua
@@ -0,0 +1,48 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.term = "wezterm"
+config.set_environment_variables = {
+  SHELL = "/bin/bash"
+}
+
+config.enable_tab_bar = false
+
+config.font = wezterm.font_with_fallback({
+  "Inconsolata Nerd Font Mono",
+  -- "Cousine Nerd Font Mono",
+  -- "Mononoki Nerd Font Mono",
+  -- "SauceCodePro Nerd Font Mono",
+  "Monaco",
+})
+config.font_size = 22
+
+config.colors = {
+  foreground = "#000000",
+  background = "#fcfcf2",
+  cursor_bg = "#000000",
+  cursor_fg = "#ffffff",
+  ansi = {
+    "black",
+    "maroon",
+    "green",
+    "olive",
+    "navy",
+    "purple",
+    "teal",
+    "silver",
+  },
+  brights = {
+    "grey",
+    "red",
+    "lime",
+    "yellow",
+    "blue",
+    "fuchsia",
+    "aqua",
+    "white",
+  },
+}
+
+return config
