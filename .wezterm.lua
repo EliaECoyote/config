6e8a2acd3a465cdfcd3fc27e1196fc72170dbec1
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2023-07-23 19:49:09 +0200

    Update wezterm fonts & colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,10 @@ config.set_environment_variables = {
 config.enable_tab_bar = false
 
 config.font = wezterm.font_with_fallback({
-  "Inconsolata Nerd Font Mono",
+  "FiraMono Nerd Font Mono",
+  "Cousine Nerd Font Mono",
+  "GoMono Nerd Font Mono",
+  "JetBrainsMono Nerd Font Mono",
   "Monaco",
 })
 config.font_size = 22
@@ -21,24 +24,24 @@ config.colors = {
   cursor_bg = "#000000",
   cursor_fg = "#ffffff",
   ansi = {
-    "black",
-    "maroon",
-    "green",
-    "olive",
-    "navy",
-    "purple",
-    "teal",
-    "silver",
+    "#000000",
+    "#d73a49",
+    "#5f8700",
+    "#999900",
+    "#005faf",
+    "#b200b2",
+    "#0087af",
+    "#e5e5e5",
   },
   brights = {
     "#666666",
-    "red",
+    "#d54e53",
     "#00b800",
     "#e5e500",
-    "blue",
-    "fuchsia",
+    "#005faf",
+    "#e500e5",
     "#0087af",
-    "white",
+    "#ffffff",
   },
 }
 
