bc083b6e38ca5a540c43f3fc2cb31f5fe3e20b73
EliaECoyote <25326494+EliaECoyote@users.noreply.github.com>
2025-10-24 10:43:11 +0200

    Add hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,47 @@
+local hs = hs
+
+local function app(name)
+  return function() hs.application.launchOrFocus(name) end
+end
+
+local function jump(browserName, url)
+  return function()
+    hs.application.launchOrFocus(browserName)
+    local script = (
+      [[(function() {
+           var browser = Application('%s');
+           browser.activate();
+
+           for (win of browser.windows()) {
+             var tabIndex = win.tabs().findIndex(function(tab) {
+              return tab.url().indexOf('%s') !== -1;
+             });
+
+             if (tabIndex != -1) {
+               win.activeTabIndex = (tabIndex + 1);
+               win.index = 1;
+             }
+           }
+         })();]]
+    ):format(browserName, url)
+    hs.osascript.javascript(script)
+  end
+end
+
+
+hs.hotkey.bind({ "ctrl", "cmd" }, "A", app("Alacritty"))
+hs.hotkey.bind({ "ctrl", "cmd" }, "D", app("Google Chrome"))
+hs.hotkey.bind({ "ctrl", "cmd" }, "S", app("IntelliJ IDEA"))
+hs.hotkey.bind(
+  { "ctrl", "cmd" },
+  "C",
+  jump("Google Chrome", "https://chatgpt.com/")
+)
+hs.hotkey.bind(
+  { "ctrl", "cmd" },
+  "S",
+  jump("Google Chrome", "https://app.slack.com/")
+)
+
+
+hs.alert.show("Hammerspoon config loaded")
